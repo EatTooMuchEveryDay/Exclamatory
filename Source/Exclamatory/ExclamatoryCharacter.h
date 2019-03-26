2a98b98773bcde2e135cca6393b8629ab7b570f9
酒酿圆子黏黏汤。 <xrui0310@126.com>
2019-03-26 13:29:23 +0800

    nothing

--- Source/Exclamatory/ExclamatoryCharacter.h
+++ Source/Exclamatory/ExclamatoryCharacter.h
@@ -22,11 +22,11 @@ public:
 	AExclamatoryCharacter();
 
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Camera)
 	float BaseTurnRate;
 
 	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Camera)
 	float BaseLookUpRate;
 
 protected:
